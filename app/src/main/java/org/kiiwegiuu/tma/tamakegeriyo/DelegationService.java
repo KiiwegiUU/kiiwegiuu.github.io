5b2c8960b705b566555874ed6527d995ba139899
KiiwegiUU <130078150+KiiwegiUU@users.noreply.github.com>
2025-06-25 11:48:40 -0400

    shrunk all text boxes

---
+++ app/src/main/java/org/kiiwegiuu/tma/tamakegeriyo/DelegationService.java
@@ -0,0 +1,14 @@
+package org.kiiwegiuu.tma.tamakegeriyo;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
