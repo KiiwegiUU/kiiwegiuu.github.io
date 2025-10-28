0ce7cdba13531688fbb8cfae4ad0aad2b6c4859d
KiiwegiUU <130078150+KiiwegiUU@users.noreply.github.com>
2025-10-28 14:33:10 -0400

    fixed master slide carryover

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Every update change in the folders needs a new v# below to update for users.  Do this after change transfered.
-const CACHE_NAME = 'tutorial-cache-v2-dynamic';
+const CACHE_NAME = 'tutorial-cache-v1-dynamic';
 const urlsToCache = [
   'manifest.json',
   // (Optional: a few files you want guaranteed offline from the start)
