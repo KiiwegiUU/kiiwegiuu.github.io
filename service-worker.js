19ebad3f4c8a12cbbf4cb65255ee73d302484a92
KiiwegiUU <130078150+KiiwegiUU@users.noreply.github.com>
2026-08-11 21:46:00 -0400

    tracking update v4

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Every update change in the folders needs a new v# below to update for users.  Do this after change transfered.
-const CACHE_NAME = 'tutorial-cache-v24-dynamic';
+const CACHE_NAME = 'tutorial-cache-v25-dynamic';
 const urlsToCache = [
   'manifest.json',
   // (Optional: add files guaranteed offline from the start below here)
